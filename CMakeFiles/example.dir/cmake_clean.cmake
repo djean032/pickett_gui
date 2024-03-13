f5d37efb94f8aa8dcd73523f4f8d04dc6f3b0c60
Dairen Jean <djean032@gmail.com>
2024-03-13 13:14:46 -0400

    fixed cmake library problems

---
+++ CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/example_autogen.dir/ParseCache.txt"
+  "example_autogen"
+  "CMakeFiles/example.dir/example_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/example.dir/example_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/example.dir/src/main.cpp.o"
+  "CMakeFiles/example.dir/src/main.cpp.o.d"
+  "CMakeFiles/example.dir/src/mainwindow.cpp.o"
+  "CMakeFiles/example.dir/src/mainwindow.cpp.o.d"
+  "example"
+  "example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
